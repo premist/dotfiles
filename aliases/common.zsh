f7ab4019abebe760f99629708e1e732dc3d3e876
Minku Lee <premist@me.com>
2016-12-17 21:58:52 +0900

    Add comment on common.zsh

--- aliases/common.zsh
+++ aliases/common.zsh
@@ -1,7 +1,13 @@
 #!/usr/bin/env zsh
+
+# Directories
 alias dev="cd $HOME/Development"
 alias shakr="cd $HOME/Development/shakr"
 
+# Heroku
+alias h="heroku"
+
+# Hub -> Git
 if [ -f "$(which hub)" ]; then
 	eval "$(hub alias -s)"
 fi
