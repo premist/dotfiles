b0aecbe45bf4e7236da2d7e0b5b6dccacf97929c
Minku Lee <premist@me.com>
2016-12-18 15:11:57 +0900

    Load chruby if exists

--- languages/ruby.zsh
+++ languages/ruby.zsh
@@ -4,5 +4,10 @@ if [ -z $CHRUBY_DIR ]; then
 	export CHRUBY_DIR=/usr/local/share/chruby
 fi
 
-source $CHRUBY_DIR/chruby.sh
-source $CHRUBY_DIR/auto.sh
+if [ -s $CHRUBY_DIR/chruby.sh ]; then
+  source $CHRUBY_DIR/chruby.sh
+fi
+
+if [ -s $CHRUBY_DIR/auto.sh ]; then
+  source $CHRUBY_DIR/auto.sh
+fi
